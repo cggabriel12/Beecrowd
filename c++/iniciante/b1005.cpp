8e0945eb6dfb654e078255d5e2029fb8957f5cbe
Carlos <gabrieljp3007@gmail.com>
2025-10-20 10:54:29 -0300

    adicicionando 03,04,05

---
+++ c++/iniciante/b1005.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    double a = 0;
+    double b = 0;
+
+    cin >> a;
+    cin >> b;
+
+    cout << fixed << setprecision(5);
+    cout << "MEDIA = " << (a * 3.5 + b * 7.5) / (3.5 + 7.5) << "\n";
+    return 0;
+}
