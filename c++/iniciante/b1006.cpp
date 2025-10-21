e1dfa576a4d7c64ed1d9f18dd8f0021883983cae
Carlos <gabrieljp3007@gmail.com>
2025-10-21 09:48:03 -0300

    adicionando 1006,1007

---
+++ c++/iniciante/b1006.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    double a = 0;
+    double b = 0;
+    double c = 0;
+
+    cin >> a >> b >> c;
+    
+
+    cout << fixed << setprecision(1);
+    cout << "MEDIA = " << (a * 2 + b * 3 + c * 5) / (2 + 3 + 5) << "\n";
+    return 0;
+}
---
+++ c++/iniciante/b1007.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    int c;
+    int d;
+
+    cin >> a >> b >> c >> d;
+
+    cout << "DIFERENCA = " << (a * b - c * d) << "\n";
+
+    return 0;
+
+}
